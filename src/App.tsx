d7f76808141d4112aab6fe47f67cf6136e388120
RODRY <rodry_valenzuela@hotmail.com>
2023-06-08 12:26:00 -0300

    funciones y estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route  } from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+import Home from "./page/home/Home"
+import About from "./page/about/About"
+import Portfolio from "./page/portfolio/Portfolio"
+import Contact from "./page/contact/Contact"
 
 
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route index element ={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="portfolio" element={<Portfolio/>}/>
+        <Route path="contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
