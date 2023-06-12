f418aac978c8082a20b63339a22de64e6c301c9b
RODRY <rodry_valenzuela@hotmail.com>
2023-06-12 20:20:58 -0300

    home terminado

--- src/page/about/About.tsx
+++ src/page/about/About.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import "./about.css";
 
 const About = () => {
     return (
-        <div>About</div>
+        <main className='about'>
+            <h2 className='section__title'>
+                About <span> Me</span>
+            </h2>
+
+        </main>
     )
 }
 
--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -27,6 +27,10 @@ const Home = () => {
                     </Link>
                 </div>
             </div>
+
+            <div className='color__block'>
+
+            </div>
         </section>
     )
 }
