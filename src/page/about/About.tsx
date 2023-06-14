8a8321a571a834a8ea90a56dbad441d9c3892e9e
RODRY <rodry_valenzuela@hotmail.com>
2023-06-14 19:32:48 -0300

    fin del about

--- src/page/about/About.tsx
+++ src/page/about/About.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Info from '../../components/Info';
 import Stats from '../../components/Stats';
 import Skills from '../../components/Skills';
+import { resume } from '../../data';
+import ResumeItem from '../../components/ResumeItem';
 import { FaDownload } from 'react-icons/fa';
 import CV from '../../assets/steve-Cv.pdf';
-import "./about.css"
+import './about.css';
 
 const About = () => {
     return (
@@ -16,20 +18,19 @@ const About = () => {
 
                 <div className='about__container grid'>
                     <div className='about__info'>
-                        <h3 className='section__subtitle'> Personal Info
-                        </h3>
+                        <h3 className='section__subtitle'> Personal Info</h3>
 
                         <ul className='info__list grid'>
-                            <Info/>
+                            <Info />
                         </ul>
 
-                        <a href={CV} download="" className='button'>
-                            Donwload CV <span className='button__icon'><FaDownload/></span>
+                        <a href={CV} download='' className='button'>
+                            Donwload CV <span className='button__icon'><FaDownload /></span>
                         </a>
                     </div>
 
                     <div className='stats grid'>
-                        <Stats/>
+                        <Stats />
                     </div>
                 </div>
             </section>
@@ -40,13 +41,35 @@ const About = () => {
                 <h3 className='section__subtitle subtitle__center'>My Skills</h3>
 
                 <div className='skills__container grid'>
-                    <Skills/>
+                    <Skills />
                 </div>
             </section>
 
             <div className='separator'></div>
+
+            <section className='resume'>
+                <h3 className='section__subtitle subtitle__center'>Experiencia y Educacion</h3>
+
+                <div className='resume__container grid'>
+                    <div className='resume__data'>
+                        {resume.map((val) => {
+                            if (val.category === 'experience') {
+                                return <ResumeItem key={val.id} {...val} />;
+                            }
+                        })}
+                    </div>
+
+                    <div className='resume__data'>
+                        {resume.map((val) => {
+                            if (val.category === 'education') {
+                                return <ResumeItem key={val.id} {...val} />;
+                            }
+                        })}
+                    </div>
+                </div>
+            </section>
         </main>
-    )
-}
+    );
+};
 
 export default About;
