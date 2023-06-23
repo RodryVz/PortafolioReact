de09679420bd285e5c735c5c449e162fc8a085a5
RODRY <rodry_valenzuela@hotmail.com>
2023-06-23 13:05:45 -0300

    fin de contact

--- src/page/contact/Contact.tsx
+++ src/page/contact/Contact.tsx
@@ -7,17 +7,85 @@ import {
 import { FiSend } from 'react-icons/fi';
 import "./contact.css";
 import "../../App.css";
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactUs = () => {
+    const form = useRef<HTMLFormElement>(null);
+    const [messageSent, setMessageSent] = useState(false);
+
+    const sendEmail = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        if (form.current !== null) {
+            emailjs
+                .sendForm(
+                    'service_yy64tyg',
+                    'template_8miui3c',
+                    form.current,
+                    'dGjvnKWRyzStBcN4g'
+                )
+                .then((result) => {
+                    console.log(result.text);
+                    setMessageSent(true);
+                }, (error) => {
+                    console.log(error.text);
+                });
+        };
+    }
+
+    return (
+        <form ref={form} onSubmit={sendEmail} className='contact__form'>
+            <div className='form__input-group'>
+                <div className='form__input-div'>
+                    <input
+                        type="text"
+                        placeholder='Tu nombre'
+                        className='form__control'
+                        name="user_name"
+                    />
+                </div>
+
+                <div className='form__input-div'>
+                    <input
+                        type="email"
+                        placeholder='Tu correo electrónico'
+                        className='form__control'
+                        name="user_email"
+                    />
+                </div>
+            </div>
+
+            <div className='form__input-div'>
+                <textarea
+                    placeholder='Tu mensaje'
+                    className='form__control textarea'
+                    name="message"
+                ></textarea>
+            </div>
+
+            <button className='button' type="submit">
+                Enviar mensaje
+                <span className='button__icon contact__button-icon'>
+                    <FiSend />
+                </span>
+            </button>
+
+            {messageSent && <p className="message-sent">¡Mensaje enviado!</p>}
+        </form>
+    );
+};
 
 const Contact = () => {
     return (
         <section className='contact section'>
             <h2 className='section__title'>
-                Get In <span>Touch</span>
+                Ponte en <span>contacto</span>
             </h2>
 
             <div className='contact__container conteiner grid'>
                 <div className='contact__data'>
-                    <h3 className='contact__title'>Don't be Shy</h3>
+                    <h3 className='contact__title'>No seas tímido</h3>
 
                     <p className='contact__description'>
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -29,7 +97,7 @@ const Contact = () => {
                         <div className='info__item'>
                             <FaEnvelopeOpen className='info__icon' />
                             <div>
-                                <span className='info__title'>Mail me </span>
+                                <span className='info__title'>Envíame un correo </span>
                                 <h4 className='info__desc'>rodry_valenzuela@hotmail.com</h4>
                             </div>
                         </div>
@@ -37,7 +105,7 @@ const Contact = () => {
                             <FaPhoneSquareAlt className='info__icon' />
 
                             <div>
-                                <span className='info__title'>Call me </span>
+                                <span className='info__title'>Llámame </span>
                                 <h4 className='info__desc'>+54 3624-968347</h4>
                             </div>
                         </div>
@@ -54,44 +122,9 @@ const Contact = () => {
                         <a href="https://facebook.com" className='contact__social-link'>
                             <FaLinkedin />
                         </a>
-
-                        <a href="https://facebook.com" className='contact__social-link'>
-                            <FaLinkedin />
-                        </a>
                     </div>
                 </div>
-
-                <form className='contact__form'>
-                    <div className='form__input-group'>
-                        <div className='form__input-div'>
-                            <input
-                                type="text"
-                                placeholder='Your Name'
-                                className='form__control' />
-                        </div>
-
-                        <div className='form__input-div'>
-                            <input type="email"
-                                placeholder='Your Email'
-                                className='form__control' />
-                        </div>
-
-                    </div>
-
-                    <div className='form__input-div'>
-                        <textarea
-                            placeholder='Your Message'
-                            className='form__control textarea'
-                            ></textarea>
-                    </div>
-
-                    <button className='button'>
-                        Send Message
-                        <span className='button__icon contact__button-icon'>
-                            <FiSend />
-                        </span>
-                    </button>
-                </form>
+                <ContactUs />
             </div>
         </section>
     )
